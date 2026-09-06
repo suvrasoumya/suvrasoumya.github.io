77fc95fdbb3e48b7b077aad334f0a0bd06092c68
suvrasoumya <45592247+suvrasoumya@users.noreply.github.com>
2026-09-06 18:48:31 +0000

    Deploying to gh-pages from @ suvrasoumya/suvrasoumya.github.io@8a8609b3fd3b21a39fbb54624ea77ab395e942c2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -26,17 +26,19 @@ ninja.data = [{
         },{id: "news-this-section-will-be-used-to-link-to-various-tools-that-i-will-build",
           title: 'This section will be used to link to various tools that I will...',
           description: "",
-          section: "News",},{id: "projects-india-markets-analysis",
-          title: 'India Markets Analysis',
-          description: "Will link my github repo here -",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{
+          section: "News",},{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
           window.open("mailto:%73%75%76%72%61%73%6F%75%6D%79%61.%6D%6F%68%61%6E%74%79@%67%6D%61%69%6C.%63%6F%6D", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/suvrasoumya-mohanty-128285ba", "_blank");
         },
       },{
         id: 'social-github',
